c1e9d0f4c2517a307e2f867f556c8ffc29856b66
Nathan <nathan.lonidier@gmail.com>
2024-06-15 09:36:55 -0400

    section 1 complete

--- Module 1 - Introduction to Java/1. Getting Started/Challenge 1/YourInitials.java
+++ Module 1 - Introduction to Java/1. Getting Started/Challenge 1/YourInitials.java
@@ -1,5 +1,22 @@
 public class YourInitials {
     public static void main(String[] args) {
          // Instructions for this workbook are on Learn the Part (See the Udemy Video: Your Initials to access the link).
+         System.out.println("NN      N    L");
+         System.out.println("N N     N    L");
+         System.out.println("N  N    N    L");
+         System.out.println("N   N   N    L");
+         System.out.println("N    N  N    L");
+         System.out.println("N     N N    L");
+         System.out.println("N      NN    L L L L L L");
+
+
+         System.out.println("G G G G G G ");
+         System.out.println("G");
+         System.out.println("G");
+         System.out.println("G");
+         System.out.println("G     G G G ");
+         System.out.println("G         G ");
+         System.out.println("G         G ");
+         System.out.println("G G G G G G ");
     }
 }
